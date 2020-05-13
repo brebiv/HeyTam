afa13c826669002533631caef9bebdf1de7d9fff
Leonid <brebiv@gmail.com>
2020-05-13 19:25:35 +0300

    Added template for news page
    Template which represents how news page looks.
    I didn't create new model yet.

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
 
+
+# Urls built for single page application usage
+# It's like very simple API
+# Example: GET index, will return only body part of page, 
+# then in Javascript I change body part of the page, to new one, that came as response to AJAX request.
+
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("index", views.main),
+    path("news", views.news, name='news'),
     path("products", views.products, name='products'),
     path("about", views.about, name="about"),
     path("<int:product_id>", views.product, name="product")
--- main/views.py
+++ main/views.py
@@ -4,7 +4,11 @@ from .models import Product, Quote
 import random
 import json
 
-# Create your views here.
+# Views built to work as single page aplication
+# index() returns header of the page, FYI it's in main/base.html
+# All other functions built to receive AJAX request from JS code 
+# They return html code for JS that put it into #main div inside base.html
+
 def index(request):
     return render(request, "main/base.html")
 
@@ -12,6 +16,9 @@ def main(request):
     quotes = Quote.objects.all()
     random_quote = random.choice(quotes)
     return render(request, 'main/main.html', {"quote": random_quote})
+
+def news(request):
+    return render(request, 'main/news.html')
 
 def products(request):
     products = [p for p in Product.objects.all()]
