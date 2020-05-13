740a75b27736d9e3dc64828dd1475f9103b08169
Leonid <brebiv@gmail.com>
2020-05-13 12:43:25 +0300

    Added optimisation to drawing feature and returned great old effect.
    Now after animation ends circle element removes. That increases performance a lot.
    Implemented dots limit, now on screen can be only up to 200 dots.
    Added cool effect, it's better to see for understanding.
    Now you can draw only when mouse button is down, not every time mouse moves, as it was before.

--- main/static/main/index.js
+++ main/static/main/index.js
@@ -7,6 +7,8 @@ function getRandomColor() {
     return color;
   }
 
+
+// Dots limit for drawing optimisation
 let dotsLimit = 200;
 let currentDots = 0;
 
@@ -21,23 +23,33 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 .attr("cy", coords[1])
                 .attr('r', 20)
                 .style('fill', "black")
-                .style('z-index', '10'); // Don't work
+            currentDots++;
+
+            if (currentDots > dotsLimit) {
+                // No more then dots then limit allows.
+                var first_dot = document.getElementById("svg").firstChild;  // Get oldest dot.
+                document.getElementById("svg").removeChild(first_dot);      // Remove oldest dot.
+                currentDots--;
+            }
+            // Remove dots when it's done animating.
+            document.querySelectorAll("circle").forEach(c => {
+                c.onanimationend = function() {
+                    c.remove();
+                    currentDots--;
+                }
+            });
         }
     }
 
     const svg = d3.select("#svg");
-    svg.on("mousemove", draw_point)
-    document.querySelectorAll("circle").forEach(c => {
-        c.style.animationPlayState = "running";
-        c.addEventListener("animationend", function () {
-            console.log("animationed")
-            this.remove();
-        });
-    })
-});
+    document.addEventListener("mousedown", ()=> {
+        // Draw while mouse is hold
+        console.log(`Currently there is - ${currentDots} on the screen`)
+        svg.on("mousemove", draw_point);
+    });
 
-document.onmousedown = () => {
-    document.querySelectorAll("circle").forEach(c => {
-        c.style.animationPlayState = "running";
-    })
-};
+    document.addEventListener("mouseup", ()=> {
+        // Stop drawing when mouse released
+        svg.on("mousemove", ()=>{}) // Callback anonymous function that does nothing. (stop drawing)
+    });
+});
