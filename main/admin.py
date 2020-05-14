a44e6e6986ac0f9c9fa3703f664ca4711f744515
Leonid <brebiv@gmail.com>
2020-05-14 11:38:40 +0300

    Added Article model for news page. Changed other models set blank=True.

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product, Quote
+from .models import Product, Quote, Article
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Quote)
+admin.site.register(Article)
--- main/models.py
+++ main/models.py
@@ -7,13 +7,16 @@ def product_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT / <title>/<filename> 
     return '{0}/{1}'.format(instance.title.lower(), filename) 
 
+def article_directory_path(instance, filename):
+    return 'news/{0}/{1}'.format(instance.title.lower(), filename)
+
 class Product(models.Model):
     title = models.CharField(max_length=64)
     short_description = models.CharField(max_length=150)
     description = models.TextField()
-    github_link = models.CharField(max_length=64, null=True)
-    product_file = models.FileField(upload_to=product_directory_path, default=None, null=True)
-    icon = models.ImageField(upload_to=product_directory_path, default=None, null=True)
+    github_link = models.CharField(max_length=64, null=True, blank=True)
+    product_file = models.FileField(upload_to=product_directory_path, default=None, null=True, blank=True)
+    icon = models.ImageField(upload_to=product_directory_path, default=None, null=True, blank=True)
     image = models.ImageField(upload_to=product_directory_path, default=None, null=True)
 
     def toDict(self) -> dict:
@@ -31,9 +34,21 @@ class Product(models.Model):
     def __str__(self):
         return f"{self.title}"
 
+
 class Quote(models.Model):
     text = models.TextField()
     author = models.CharField(max_length=64)
 
     def __str__(self):
         return f"{self.text} --- {self.author}"
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=64)
+    main_text = models.TextField()
+    image = models.ImageField(upload_to=article_directory_path, default=None, null=True, blank=True)
+    more_link = models.CharField(max_length=64, default=None, null=True, blank=True)
+    pub_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.pub_date}"
