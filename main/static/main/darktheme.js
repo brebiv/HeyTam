f8684abc5f7d76f7aece95fb6df728771ac147bb
Leonid <brebiv@gmail.com>
2020-05-14 22:05:44 +0300

    Added ability swith back to bright theme
    But transition isn't smooth tough. Should fix it later.

--- main/static/main/darktheme.js
+++ main/static/main/darktheme.js
@@ -3,15 +3,33 @@ function makeItDark() {
         $("#theme_css").attr('href', STATIC_PATH+"darktheme.css");
         $("html").fadeIn("slow", function() {
             $("#logo").attr('src', STATIC_PATH+"logo_heytam_red.png");
-            $("#welcome-text").html("Welcome to Dark HeyTam!")
+            $("#welcome-text").html("Welcome to Dark HeyTam!");
+            $("#change-theme").html("Light");
+            $("#change-theme").data('theme', 'dark');
         });
     });
 }
 
+function makeItBright() {
+    $("html").fadeOut("slow", function() {
+        $("#theme_css").attr('href', '');
+        $("html").fadeIn("slow", function() {
+            $("#logo").attr('src', STATIC_PATH+"logo_heytam.png");
+            $("#welcome-text").html("Welcome back!");
+            $("#change-theme").html("Dark");
+            $("#change-theme").data('theme', 'bright');
+        });
+    });
+}
 
 $(document).ready(function() {
     $("#change-theme").click(function() {
-        makeItDark();
+        let currentTheme = $(this).data('theme');
+        if (currentTheme == 'bright') {
+            makeItDark();
+        } else {
+            makeItBright();
+        }
         return false;
     });
 })
